e9e3872dcb01b54e0f83c14e808f2f4c75852929
ibrahimelgazwy <46788851+IbrahimElgazwy@users.noreply.github.com>
2021-11-27 22:07:48 +0200

    refact pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -5,13 +5,15 @@ const Pagination = ({paging, onLoad}) => {
            hasNext, hasPrevous, totalPages} = paging;
     return ( 
         <div className="pagination">
-            <div className="page" onClick={() => hasPrevous ? onLoad(prevousPage) : void(0)}>
+            <div className="page" 
+                 onClick={() => hasPrevous ? onLoad(prevousPage) : void(0)}>
                 <i className="fa fa-chevron-left"></i>
             </div>
             <div className="page page_content">
                 <span> {`${currentPage + 1} of ${totalPages}`} </span>
             </div>
-            <div className="page" onClick={() => hasNext ? onLoad(nextPage) : void(0)}>
+            <div className="page" 
+                 onClick={() => hasNext ? onLoad(nextPage) : void(0)}>
                 <i className="fa fa-chevron-right"></i>
             </div>
         </div>
